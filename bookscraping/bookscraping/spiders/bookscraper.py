4ae4fd03ec7d5b9610f71258c4001777c61bc483
Timiemmy <faith.bolanle@yahoo.com>
2025-04-09 05:17:53 +0100

    first scraping

--- bookscraping/bookscraping/spiders/bookscraper.py
+++ bookscraping/bookscraping/spiders/bookscraper.py
@@ -7,4 +7,15 @@ class BookscraperSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+            title = book.css('h3 a::text').get()
+            price = book.css('div.product_price .price_color::text').get()
+            url = book.css('h3 a').attrib['href'],
+
+            yield {
+                'title': title,
+                'price': price,
+                'url': url,
+            }
